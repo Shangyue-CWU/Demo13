da51825d6ca2fffa36036dd1dd9995f660c359d3
Shangyue-CWU <zhush@cwu.edu>
2023-09-28 11:18:26 -0700

    Add new files

---
+++ Test1.java
@@ -0,0 +1,13 @@
+
+public class TestSuite {
+	public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        
+    }
+
+	
+}
+public class Cat{
+    private String Name;
+}
