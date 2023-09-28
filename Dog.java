616cd970d08215a502a398f8b844cfc20f20faa4
Shangyue-CWU <144855209+Shangyue-CWU@users.noreply.github.com>
2023-09-28 11:35:27 -0700

    Update Dog.java
    Add hello 380

--- Dog.java
+++ Dog.java
@@ -3,6 +3,8 @@ public class TestSuite {
 	public static void main(String[] args) {
         System.out.println("Hello, Dog!");
 
+	System.out.println("Hello, 380!");
+
         
     }
 
@@ -13,6 +15,7 @@ public class Dog{
     private int Age;
 
     public Dog(){
+	    
 
     }
 }
